7cad0f1dfc1329e6c22b8ddd455d9a6e73a5fda7
vivek anjana <vivekanjana76@gmail.com>
2023-09-29 15:53:12 +0530

    export-import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Promo from "./components/Promo";
+import Intro1 from "./components/Intro1";
+import Intro2 from "./components/Intro2";
+import Intro3 from "./components/Intro3";
 
-function Header() {
-  return <h1>Hello world</h1>;
-}
 function App() {
-  return <Header />;
+  return (
+    <div className="App">
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
