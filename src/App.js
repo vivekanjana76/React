b86c5447485d375db8b2f0d4f731afac7cbb5975
vivek anjana <vivekanjana76@gmail.com>
2023-10-03 14:36:42 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import MealsProvider from "./providers/MealsProvider";
-import MealsList from "./components/MealsList";
-import Counter from "./components/Counter";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action.type === 'ride') return {money: state.money + 10};
+  if (action.type === 'fuel') return {money: state.money - 50};
+  return new Error();
+}
 
 function App() {
+  const initialState = {money: 100};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div>
-      <MealsProvider>
-        <MealsList />
-        <Counter />
-      </MealsProvider>
+    <div className="App">
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={()=>dispatch({ type:'ride'})}>A new customer!</button><br/>
+        <button onClick={()=>dispatch({ type:'fuel'})}>Refill the tank!</button><br/>
+      </div>
     </div>
   );
 }
-  
-  export default App;
+
+export default App;
